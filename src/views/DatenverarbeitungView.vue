52e6f391bab43c4e9ce215b691964c10e7f0262d
Janosch Schwalm <janosch.schwalm@eb.de>
2022-08-18 10:37:50 +0200

    data tabel genutz, um gruppen in der darstellung zunutzen

--- src/views/DatenverarbeitungView.vue
+++ src/views/DatenverarbeitungView.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/valid-v-slot -->
 <template>
   <v-container>
     <v-row>
@@ -9,51 +10,57 @@
     </v-row>
     <v-row>
       <v-col cols="12">
-        <v-simple-table>
-          <thead>
-            <tr>
-              <th class="text-left">Name</th>
-              <th class="text-left">Kosten pro Lizenz</th>
-              <th></th>
-              <th class="text-left">Gesamtkosten</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, index) in licenses" :key="index">
-              <td>{{ item.name }}</td>
-              <td>{{ item.einzelkosten }}{{ " €" }}</td>
-              <td>
-                <v-autocomplete
-                  v-model="antworten[item.name]"
-                  label="Anzahl der Lizenzen"
-                  hint="sowohl aktiv genutzt, als auch passiv"
-                  auto-select-first
-                  :items="[...Array(16).keys()]"
-                >
-                </v-autocomplete>
-              </td>
-              <td>
-                {{ item.einzelkosten * antworten[item.name] || 0 }}{{ " €" }}
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                Kosten aller Lizenzen:
-                <br />
-                {{ kostenAllerLizenzen() }}{{ " €" }}
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
+        <v-data-table
+          :headers="lizenzHeader"
+          :items="lizenzenMitGruppen"
+          :search="lizenzeSearch"
+          :items-per-page="5"
+          :group-by="sachkostenunterart"
+        >
+          <template v-slot:top>
+            <v-text-field
+              v-model="lizenzeSearch"
+              append-icon="mdi-magnify"
+              label="Suche"
+              single-line
+              hide-details
+            >
+            </v-text-field>
+          </template>
+
+          <template v-slot:item.Anzahl="{ item }">
+            <v-autocomplete
+              v-model="antworten[item[lizenzname]]"
+              label="Anzahl der Lizenzen"
+              hint="sowohl aktiv genutzt, als auch passiv"
+              auto-select-first
+              :items="[...Array(16).keys()]"
+            >
+            </v-autocomplete>
+          </template>
+          <template v-slot:item.Gesamtkosten="{ item }">
+            {{ item[kostenProLizenz] * antworten[item[lizenzname]] || 0
+            }}{{ " €" }}
+          </template>
+
+          <template v-slot:footer>
+            <p class="text-right">
+              {{ `Kosten aller Lizenzen: ${kostenAllerLizenzen()} €` }}
+            </p>
+          </template>
+        </v-data-table>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+const lizenzname = "Lizenzname";
+const kostenProLizenz = "Kosten pro Lizenz";
+const anzahl = "Anzahl";
+const gesamtkosten = "Gesamtkosten";
+const sachkostenunterart = "Sachkostenunterart";
+
 export default {
   methods: {
     kostenAllerLizenzen() {
@@ -63,19 +70,76 @@ export default {
     },
   },
   data: () => ({
+    lizenzeSearch: null,
+    lizenzname,
+    kostenProLizenz,
+    anzahl,
+    gesamtkosten,
+    sachkostenunterart,
     antworten: {},
+    lizenzHeader: [
+      { text: lizenzname, value: lizenzname },
+      { text: kostenProLizenz, value: kostenProLizenz },
+      { text: anzahl, value: anzahl },
+      { text: gesamtkosten, value: gesamtkosten },
+    ],
+
+    lizenzenMitGruppen: [
+      {
+        [lizenzname]: "Asana",
+        [kostenProLizenz]: 13.49 * 12,
+        [sachkostenunterart]: "Softwarelizenzen Drittanbieter",
+      },
+      {
+        [lizenzname]: "agree21VPN",
+        [kostenProLizenz]: 19 * 12,
+        [sachkostenunterart]: "Softwarelizenzen Atruvia",
+      },
+      {
+        [lizenzname]: "Basispaket Cloud",
+        [kostenProLizenz]: 212.64,
+        [sachkostenunterart]: "Softwarelizenzen Atruvia",
+      },
+      {
+        [lizenzname]: "Microsoft Office 365",
+        [kostenProLizenz]: 140.64,
+        [sachkostenunterart]: "Softwarelizenzen Drittanbieter",
+      },
+      {
+        [lizenzname]: "Doksharing",
+        [kostenProLizenz]: 30,
+        [sachkostenunterart]: "Softwarelizenzen Atruvia",
+      },
+      {
+        [lizenzname]: "Bloomberg Terminal",
+        [kostenProLizenz]: 20_000,
+        [sachkostenunterart]: "Leitungskosten Miet und Wartungsscheine",
+      },
+    ],
     licenses: [
       {
-        name: "Asana",
-        einzelkosten: 123,
+        [lizenzname]: "Asana",
+        [kostenProLizenz]: 13.49 * 12,
       },
       {
-        name: "agree21VPN",
-        einzelkosten: 37,
+        [lizenzname]: "agree21VPN",
+        [kostenProLizenz]: 19 * 12,
       },
       {
-        name: "Basispaket Cloud",
-        einzelkosten: 45,
+        [lizenzname]: "Basispaket Cloud",
+        [kostenProLizenz]: 212.64,
+      },
+      {
+        [lizenzname]: "Microsoft Office 365",
+        [kostenProLizenz]: 140.64,
+      },
+      {
+        [lizenzname]: "Doksharing",
+        [kostenProLizenz]: 30,
+      },
+      {
+        [lizenzname]: "Bloomberg Terminal",
+        [kostenProLizenz]: 20_000,
       },
     ],
   }),
